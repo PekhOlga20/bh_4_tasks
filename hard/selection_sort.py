f78f29530be5a98a6bec44cc446750f8c39aa673
Ольга <pekh@gmail.com>
2021-03-29 00:47:37 +0300

    29.03.2021

--- hard/selection_sort.py
+++ hard/selection_sort.py
@@ -19,7 +19,14 @@
 
 
 def selection_sort(array: list) -> list:
-    pass
+    for i in range(len(array) -1):
+        lowest_value_index = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[lowest_value_index]:
+                lowest_value_index = j
+        array[i], array[lowest_value_index] = array[lowest_value_index], array[i]
+
+    return array
 
 
 if __name__ == '__main__':
